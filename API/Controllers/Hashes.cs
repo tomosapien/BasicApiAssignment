fcd4c2edff62e6410922623942e70f390033452f
Tom Smit <tomosapien_66@hotmail.com>
2023-01-21 23:18:17 +0100

    change hash method and launchsettings

--- API/Controllers/Hashes.cs
+++ API/Controllers/Hashes.cs
@@ -7,9 +7,13 @@ namespace API.Controllers
         public HashesController()
         {}
 
-        [HttpPost("{data}")]
-        public ActionResult<UInt32> GetHash(UInt32 data) {
-            return data;
+        [HttpPost]
+        public ActionResult<UInt64> GetHash(string data) {
+            UInt64 hash = 3461;
+            foreach (char c in data) {
+                hash = (c + hash) * c;
+            }
+            return hash;
         }
     }
 }
